de856606016ae85de0914e80424bce59505d7753
mauricioinaz <mauricio.glez@gmail.com>
2019-11-13 16:52:15 -0500

    last updates

--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -2,11 +2,14 @@ import React, {Component} from 'react';
 import {NavLink} from "react-router-dom";
 import cl from './Layout.module.css';
 import pdf from '../../assets/pdfs/MauricioCV2019.pdf'
+import tw from '../../assets/icons/tw.png'
+import cv from '../../assets/icons/cvIc.png'
 //import DashToolbar from '../../components/Navigation/DashToolbar/DashToolbar';
 
 class Layout extends Component {
   render() {
 
+    //<NavLink to="/redes" exact activeClassName={cl.active}>Redes</NavLink>
     return (
       <>
       <header className={cl.Toolbar}>
@@ -14,10 +17,18 @@ class Layout extends Component {
           <div>
             <ul className={cl.NavigationItems}>
               <li className={cl.NavigationItem}>
-                <NavLink to="/redes" exact activeClassName={cl.active}>Redes</NavLink>
+                <div className={cl.Tuirer}>
+                  <a href={"https://twitter.com/mauricioinaz"}>
+                    <img src={tw} alt="tuirer"/>
+                  </a>
+                </div>
               </li>
               <li className={cl.NavigationItem}>
-                <a href={pdf}>CV</a>
+                <div className={cl.Tuirer}>
+                  <a href={pdf}>
+                    <img src={cv} alt="tuirer"/>
+                  </a>
+                </div>
               </li>
             </ul>
             </div>
